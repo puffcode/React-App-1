e9c68fec9448def9ff5bbb859c7c547ef57b12ec
puffcode <74677679+puffcode@users.noreply.github.com>
2025-02-03 17:38:09 -0500

    Removed extra comments

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,24 +6,15 @@ import { useState, useCallback } from "react";
  */
 export default function Sidebar() {
   let [newMenuItem, setNewMenuItem] = useState("");
-  // TODO: 2 Using a state hook, maintain the current menu items as an array state.
   let [menuItems, setMenuItems] = useState([]);
   let [filter, setFilter] = useState("");
-  // Adds a single string passed in as parameter to the state element
-  // "menuItems" that holds the set of current menu items.
+
   let addMenuItem = useCallback(() => {
     setMenuItems([...menuItems, newMenuItem]);
     setNewMenuItem("");
     console.log("Added menu item");
-    //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
-    //   // This involves adding a parameter and changing a class instance variable (props).
   }, [newMenuItem, menuItems]);
 
-  // TODO: 4. Display ONLY the menu items that contain the filter element value
-  // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
-
-  // TODO: 1 Render inside the outer div an unordered list of the menu items, with each string in the array
-  // its own item.
   return (
     <div>
       <ul>
